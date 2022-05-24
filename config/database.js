8f67ea06b887ea3aae47de603c0b2476731e7208
FindlayJosh <joshuafindlay@gmail.com>
2022-05-24 16:12:34 -0400

    Beginning of savage mode, thumbs works

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://JoshuaFindlay:Password@cluster0.o9tpc.mongodb.net/Cluster0?retryWrites=true&w=majority', 
+    'dbName': 'demo'
+};
